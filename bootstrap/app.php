5c4c6c3d1204cb13c427501b9215c62cb6c2ce0c
Dario Casertano <dario.casertano@gmail.com>
2025-06-26 15:56:32 +0200

    Carica variabili di ambiente da file .env e .env.development.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,22 @@
 <?php
 
+use Dotenv\Dotenv;
+use Dotenv\Repository\RepositoryBuilder;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
+$repository = RepositoryBuilder::createWithDefaultAdapters()
+    ->immutable()
+    ->make();
+
+$dotenv = Dotenv::create($repository, __DIR__ . '/../', '.env');
+$dotenv->load();
+
+if (file_exists(__DIR__ . '/../.env.development')) {
+    $dotenvDev = Dotenv::create($repository, __DIR__ . '/../', '.env.development');
+    $dotenvDev->load();
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
